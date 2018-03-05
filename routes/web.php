3aaaa5a57584ea4830f1d7db7293a7ff1c02e5b1
unknown <daveadjei@yahoo.co.uk>
2018-03-05 10:34:47 +0100

    up till routing1

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage');
+})->name('Homepage');
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('Homepage');
+// Route::get('/home', 'HomeController@index')->name('Homepage');
